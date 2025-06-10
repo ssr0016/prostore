fd110f3370bbadcf777bdb9a1a063d8530ff3e14
ssr0016 <samsonrecluta0016@gmail.com>
2025-06-10 08:59:24 +0800

    Credentials Sign In Form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -9,6 +9,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { APP_NAME } from "@/lib/constants";
 import { Metadata } from "next";
+import CredentiansSignInFrom from "./credentials-signin-form";
 
 export const metadata: Metadata = {
   title: "Sign In",
@@ -33,7 +34,9 @@ const SignInPage = () => {
             Sign in to your account
           </CardDescription>
         </CardHeader>
-        <CardContent className="space-y-4">{/* FORM HERE  */}</CardContent>
+        <CardContent className="space-y-4">
+          <CredentiansSignInFrom />
+        </CardContent>
       </Card>
     </div>
   );
