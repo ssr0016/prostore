98474e868a07023bedcf7eab310e9fbc1812ec22
ssr0016 <samsonrecluta0016@gmail.com>
2025-06-10 15:28:11 +0800

    Customize Token With JWT Callback

--- auth.ts
+++ auth.ts
@@ -3,7 +3,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/db/prisma";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compareSync } from "bcrypt-ts-edge";
-import type { NextAuthConfig } from "next-auth"; 
+import type { NextAuthConfig } from "next-auth";
 
 export const config = {
   pages: {
@@ -54,16 +54,36 @@ export const config = {
     }),
   ],
   callbacks: {
-    async session({ session,  user, trigger, token }: any) {
-        // Set the user ID from the token
-        session.user.id = token.sub;
+    async session({ session, user, trigger, token }: any) {
+      // Set the user ID from the token
+      session.user.id = token.sub;
+      session.user.role = token.role;
+      session.user.name = token.name;
 
-        // If the is an update, set the user name
-         if(trigger === 'update') {
-            session.user.name = user.name;
-         }
-        
-        return session;
+      // If the is an update, set the user name
+      if (trigger === "update") {
+        session.user.name = user.name;
+      }
+
+      return session;
+    },
+    async jwt({ token, user, trigger, session }: any) {
+      // Assign user fields to token
+      if (user) {
+        token.role = user.role;
+
+        // if user has no name then use the email
+        if (user.name === "NO_NAME") {
+          token.name = user.email!.split("@")[0];
+
+          // Update database to reflect the token name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          });
+        }
+      }
+      return token;
     },
   },
 } satisfies NextAuthConfig;
