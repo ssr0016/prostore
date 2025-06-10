4b500a5217995151804bab7f5017ef708f425c98
ssr0016 <samsonrecluta0016@gmail.com>
2025-06-10 08:14:04 +0800

    Sign In And Sign Out Action

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,33 @@
+"use server";
+import { signInFormSchema } from "../validator";
+import { signIn, signOut } from "@/auth";
+import { isRedirectError } from 'next/dist/client/components/redirect-error';
+
+// Sign in user with credentials
+export async function signInWithCredentials(
+  prevState: unknown,
+  formData: FormData
+) {
+    try {
+        const user = signInFormSchema.parse({
+            email: formData.get("email") as string,
+            password: formData.get("password") as string
+        });
+
+        await signIn("credentials", user);
+
+        return { success: true, message: "Signed in successfully" };
+    } catch (error) {
+        if(isRedirectError(error)){
+            throw error;
+        }
+
+        return { success: false, message: "Invalid email or password" };
+    }
+}
+
+// Sign user out
+export async function signOutUser() {
+    await signOut();
+    
+}
